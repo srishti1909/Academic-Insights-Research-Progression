2293892a0842bb1cb3dac7cab8c5d0bd284512e5
srishti1909 <144073291+srishti1909@users.noreply.github.com>
2025-02-16 13:56:41 -0600

    Add files via upload

---
+++ config/mysql_config.py
@@ -0,0 +1,6 @@
+# Replace USERNAME and PASSWORD with your MySQL username and password below:
+MYSQL_USERNAME = 'root'
+MYSQL_PASSWORD = 'ENTER_YOUR_PASSWORD_HERE'
+MYSQL_DB_NAME = 'academicworld'
+MYSQL_HOST = 'localhost'
+MYSQL_PORT = '3306'
