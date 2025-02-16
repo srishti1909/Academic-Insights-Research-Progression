3ec7723da626fc49f159f40f441000813a3baca8
srishti1909 <144073291+srishti1909@users.noreply.github.com>
2025-02-16 13:57:07 -0600

    Add files via upload

---
+++ config/mongodb_config.py
@@ -0,0 +1,4 @@
+# Replace HOST and PORT with your MongoDB host and port below:
+MONGO_HOST = 'localhost'
+MONGO_PORT = '27017'
+MONGO_DB_NAME = 'academicworld'
---
+++ config/neo4j_config.py
@@ -0,0 +1,6 @@
+# Replace USERNAME and PASSWORD with your Neo4j username and password below:
+NEO4J_USERNAME = "neo4j"
+NEO4J_PASSWORD = "ENTER_YOUR_PASSWORD_HERE"
+NEO4J_DB_NAME = "academicworld"
+NEO4J_HOST = "localhost"
+NEO4J_PORT = "7687"
